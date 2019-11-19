7a52e6dc09a5b5e3b94dd837e9eeb7dc146aa6e6
Rahul Gala <rgala38@gmail.com>
2019-11-19 13:11:44 +0530

    Finished SignIn and SignUp methods

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -5,7 +5,7 @@ import './sign-up.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {suth, createUserProfileDocument} from '../../firebase/firebase.utils';
+import {createUserProfileDocument} from '../../firebase/firebase.utils';
 import { auth } from 'firebase';
 
 class SignUp extends React.Component{
